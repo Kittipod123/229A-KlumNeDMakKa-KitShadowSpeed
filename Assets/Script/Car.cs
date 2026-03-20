dc933344c4a5a411edf5f52cf958146c5e88b26c
KITTIPOD0 <tetezaza007777@gmail.com>
2026-03-20 22:09:51 +0700

    car_run

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Car : MonoBehaviour
 {
+
+    [SerializeField] float speed =2.0f;
+    [SerializeField] float speedTurn =1.0f;
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()//ขยับตามช่วงเวลา
     {
-        
+        float turn = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector3(turn * speed, rb.linearVelocity.x, speed);
+        Debug.Log(rb.linearVelocity);
     }
 }
